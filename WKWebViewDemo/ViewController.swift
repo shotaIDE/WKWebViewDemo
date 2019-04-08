d6dc36fd8aa5d39994932e2a9bd10fec9b7c39b3
shota.ide <shota.ide@daj.co.jp>
2019-04-08 18:55:37 +0900

    Add reload button

--- WKWebViewDemo/ViewController.swift
+++ WKWebViewDemo/ViewController.swift
@@ -14,12 +14,17 @@ class ViewController: UIViewController {
     let targetUrl = "https://www.google.co.jp/"
 
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var reloadButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let urlRequest = URLRequest(url:URL(string:targetUrl)!)
         webView.load(urlRequest)
+    }
+
+    @IBAction func reload(_ sender: Any) {
+        webView.reload()
     }
 }
 
