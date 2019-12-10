60b3b5ca62e68a497834ee9873a6ee8f2050246d
shotaIDE <ide.shota.dev@gmail.com>
2019-12-11 00:40:54 +0900

    Add UI test with mock server

--- WKWebViewDemoUITests/WKWebViewDemoUITests.swift
+++ WKWebViewDemoUITests/WKWebViewDemoUITests.swift
@@ -1,34 +1,27 @@
-//
-//  WKWebViewDemoUITests.swift
-//  WKWebViewDemoUITests
-//
-//  Created by Shota Ide on 2019/04/06.
-//  Copyright © 2019 Shota Ide. All rights reserved.
-//
-
 import XCTest
+import Swifter
 
 class WKWebViewDemoUITests: XCTestCase {
+    let server = HttpServer()
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        server["/"] = { request in
+            return HttpResponse.ok(.text("<a href=\"\">Sample Link</a>"))
+        }
+        try? server.start(9080)
+
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let app = XCUIApplication()
 
+        let urlInputField = app.textFields["URL Input Field"]
+        urlInputField.tap()
+        urlInputField.typeText("http://localhost:9080/\n")
+
+        XCTAssert(app.webViews.links["Sample Link"].isEnabled)
+    }
 }
